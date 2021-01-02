b56675809e185f479ea42eef2d5a5326cba1e9b8
mohamaddiwany <67122485+mohamaddiwany@users.noreply.github.com>
2021-01-02 17:45:22 +0200

    Update seetree.py

--- seetree.py
+++ seetree.py
@@ -68,4 +68,4 @@ def starting_work(IMAGE_FILE_NAME,FUNC_NAME):
 
 #If we run it using seetree.py not throw flask we get it on port 80
 if __name__ == "__main__":
-    app.run(debug=True , host="0.0.0.0" , port=80)
+    app.run(debug=True , host="127.0.0.1" , port=80)
