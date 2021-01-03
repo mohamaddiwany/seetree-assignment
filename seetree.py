04122ee6baed94b65995fc709d4ff1e53d0fecc1
mohamaddiwany <67122485+mohamaddiwany@users.noreply.github.com>
2021-01-03 17:18:52 +0200

    Add files via upload

--- seetree.py
+++ seetree.py
@@ -8,7 +8,7 @@ import imagestat as istat
 app = Flask(__name__)
 
 
-my_dict = {}
+image_data_dict = {}
 
 #the main route 127.0.0.1:5000
 @app.route('/', methods=['GET'])
@@ -35,8 +35,8 @@ def starting_work(IMAGE_FILE_NAME,FUNC_NAME):
     mean_message = ' Median pixel level for each band in the image is'
     percintle_message = ' {} percentile of the image is'.format(FUNC_NAME[1:])
 
-    if IMAGE_FILE_NAME in my_dict:
-            if FUNC_NAME in my_dict[IMAGE_FILE_NAME]:
+    if IMAGE_FILE_NAME in image_data_dict:
+            if FUNC_NAME in image_data_dict[IMAGE_FILE_NAME]:
                 if FUNC_NAME == 'min':
                     message = min_message
                 if FUNC_NAME == 'max':
@@ -48,7 +48,7 @@ def starting_work(IMAGE_FILE_NAME,FUNC_NAME):
                 if FUNC_NAME[0] == 'p':
                     message = percintle_message
 
-                return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = my_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =message)
+                return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =message)
     #Make Sure the FUNC_NAME is one of the supported function
     if FUNC_NAME != 'min' and FUNC_NAME != 'max' and FUNC_NAME != 'mean' and FUNC_NAME != 'median':
         if FUNC_NAME[0] !='p' or  not FUNC_NAME[1:].isnumeric() or int(FUNC_NAME[1:])< 0 or int(FUNC_NAME[1:]) >100:
@@ -71,32 +71,32 @@ def starting_work(IMAGE_FILE_NAME,FUNC_NAME):
     stat = ImageStat.Stat(img)
     #check which function is needed to work and return the correct answer
     if FUNC_NAME == 'min':
-        my_dict[IMAGE_FILE_NAME]={}
-        my_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.minimum(istat,stat,IMAGE_FILE_NAME)
+        image_data_dict[IMAGE_FILE_NAME]={}
+        image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.minimum(istat,stat,IMAGE_FILE_NAME)
         # return istat.imagestat.minimum(istat,stat,IMAGE_FILE_NAME)
-        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = my_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =min_message)
+        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =min_message)
 
 
     if FUNC_NAME == 'max':
-        my_dict[IMAGE_FILE_NAME]={}
-        my_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.maximum(istat,stat,IMAGE_FILE_NAME)
-        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = my_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =max_message)
+        image_data_dict[IMAGE_FILE_NAME]={}
+        image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.maximum(istat,stat,IMAGE_FILE_NAME)
+        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =max_message)
 
 
     if FUNC_NAME == 'mean':
-        my_dict[IMAGE_FILE_NAME]={}
-        my_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.mean(istat,stat,IMAGE_FILE_NAME)
-        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = my_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =mean_message)
+        image_data_dict[IMAGE_FILE_NAME]={}
+        image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.mean(istat,stat,IMAGE_FILE_NAME)
+        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =mean_message)
         
     if FUNC_NAME == 'median':
-        my_dict[IMAGE_FILE_NAME]={}
-        my_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.median(istat,stat,IMAGE_FILE_NAME)
-        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = my_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =median_message)
+        image_data_dict[IMAGE_FILE_NAME]={}
+        image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.median(istat,stat,IMAGE_FILE_NAME)
+        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =median_message)
 
     if FUNC_NAME[0] == 'p':
-        my_dict[IMAGE_FILE_NAME]={}
-        my_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.percentile(istat,img,IMAGE_FILE_NAME,FUNC_NAME)
-        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = my_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =percintle_message)
+        image_data_dict[IMAGE_FILE_NAME]={}
+        image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] = istat.imagestat.percentile(istat,img,IMAGE_FILE_NAME,FUNC_NAME)
+        return render_template("index3.html", user_image = "local-filename.jpg" ,IMG_NAME= IMAGE_FILE_NAME , value = image_data_dict[IMAGE_FILE_NAME][FUNC_NAME] , text =percintle_message)
         
 
 #If we run it using seetree.py not throw flask we get it on port 80
